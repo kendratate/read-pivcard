f8031f74400a700419af43fbc28db89b7c0ca0e8
Tyler.Simpson <Tyler.Simpson@TSIMPSON19470>
2021-02-27 11:57:08 -0700

    first commit - ts

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+});
+
+app.allowRendererProcessReuse = false;
